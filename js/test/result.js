1d5e9a487f15510f0ee8aa340c77a80e8dfb5cbc
Ruslan Polonkoev <suslik87@gmail.com>
2018-04-27 16:04:15 +0300

    пытаюсь успокоить трэвиса

---
+++ js/test/result.js
@@ -0,0 +1,31 @@
+const playerPlaceEndings = (num) => {
+  let ending;
+  if (num % 100 === 13) {
+    ending = `-ое`;
+  } else if (num % 10 === 3) {
+    ending = `-е`;
+  } else {
+    ending = `-ое`;
+  }
+  return ending;
+};
+
+const showPlayerResult = (arr, playerResult) => {
+  if (playerResult.lifeCount === 0) {
+    return `У вас закончились все попытки. Ничего, повезёт в следующий раз!`;
+  } else if (playerResult.timeCount === 0) {
+    return `Время вышло! Вы не успели отгадать все мелодии`;
+  } else {
+    const results = [...arr];
+    results.push(playerResult.points);
+    results.sort((a, b) => {
+      return b - a;
+    });
+    const playersCount = results.length;
+    const playerPlace = results.indexOf(playerResult.points) + 1;
+    const playerRating = Math.floor((playersCount - playerPlace) / playersCount * 100);
+    return `Вы заняли ${playerPlace}${playerPlaceEndings(playerPlace)} место из ${playersCount} игроков. Это лучше, чем у ${playerRating}% игроков`;
+  }
+};
+
+export default showPlayerResult;
